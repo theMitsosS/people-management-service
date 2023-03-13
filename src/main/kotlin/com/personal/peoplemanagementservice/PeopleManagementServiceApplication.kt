985dd0c9b7d914171e2b7de1a5de4e5075675fb8
Dimitris Selalmazidis <Dimitris.Selalmazidis1@aexp.com>
2023-03-13 19:54:51 +0000

    first commit

---
+++ src/main/kotlin/com/personal/peoplemanagementservice/PeopleManagementServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.personal.peoplemanagementservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PeopleManagementServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<PeopleManagementServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/personal/peoplemanagementservice/PeopleManagementServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.personal.peoplemanagementservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PeopleManagementServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
